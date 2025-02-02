6625aab8ec64d27a341d069b77bbafc899e2bd5d
mohitadhikari123 <111336786+mohitadhikari123@users.noreply.github.com>
2025-02-02 16:44:56 +0530

    Refactor comment and video controllers; implement comment addition logic and validate video publishing fields; update middleware import paths

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -8,10 +8,17 @@ const getVideoComments = asyncHandler(async (req, res) => {
   //TODO: get all comments for a video
   const { videoId } = req.params;
   const { page = 1, limit = 10 } = req.query;
+
+
 });
 
 const addComment = asyncHandler(async (req, res) => {
   // TODO: add a comment to a video
+
+  const { content } = req.body;
+  const { videoId } = req.params;
+  const owner = req.user._id;
+  
 });
 
 const updateComment = asyncHandler(async (req, res) => {
--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -9,11 +9,45 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 const getAllVideos = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
   //TODO: get all videos based on query, sort, pagination
+  // const video = await Video.findById(userId);
+
+  // if (!video) {
+  //   throw new ApiError(404, "No video found");
+  // }
 });
 
 const publishAVideo = asyncHandler(async (req, res) => {
   const { title, description } = req.body;
+// console.log('req.body', req.files)
+// console.log('req.user', req.user)
   // TODO: get video, upload to cloudinary, create video
+
+  //validate title and description fields are not empty
+
+  //get local path of video file and thumbnail file
+  //validate video and thumbnail file upload by multer middleware
+
+  //upload video file and thumbnail file to cloudinary
+  // validate video and thumbnail file upload by on clodinary
+
+  //crate a Video document in the database
+  // assign all the fields to the video document
+  // check the video is created or not in the database
+  // return the response
+
+
+  if(!title || !description) {
+    throw new ApiError(400, "Title and description are required");
+  }
+
+  const localVideoPath = req.files?.videoFile[0].path;
+  const localThumbnailPath = req.files?.thumbnail[0].path;
+
+  if(!localVideoPath || !localThumbnailPath) {
+    throw new ApiError(400, "Video and thumbnail are required");
+  }
+
+res.status(201).json({message: "Video published successfully"});
 });
 
 const getVideoById = asyncHandler(async (req, res) => {
